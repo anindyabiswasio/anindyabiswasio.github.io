783069eba6f728f2e74df06e54440eaeedde870e
Anindya Biswas <myself@anindyabiswas.io>
2018-06-13 21:35:04 +0530

    Trying to get projects to work
    Test

--- js/site.js
+++ js/site.js
@@ -3,13 +3,14 @@
 
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
-				myFunctionBlog(this.responseText);
+				myFunctionBlogs(this.responseText);
+				myFunctionProjects(this.responseText);
 			}
 		}
 		xmlhttp.open("GET", blogurl, true);
 		xmlhttp.send();
 
-		function myFunctionBlog(response) {
+		function myFunctionBlogs(response) {
 			var arr = JSON.parse(response);
 			var i;
 			var out = "";
@@ -24,4 +25,21 @@
 						+ "</div></div></div></div>";
 			}
 			document.getElementById("blogblock").innerHTML = out;
+		}
+
+		function myFunctionProjects(response) {
+			var arr = JSON.parse(response);
+			var i;
+			var out = "";
+			for (i = 0; i < arr.length; i++) {
+
+				out += "<div><h5 class=\"header\">"
+						+ arr[i].Title
+						+ "</h5><h6>"
+						+ arr[i].Date
+						+ "</h6><div class=\"card horizontal\"><div class=\"card-stacked\"><div class=\"card-content\">"
+						+ decodeURI(arr[i].BlogEncoded)
+						+ "</div></div></div></div>";
+			}
+			document.getElementById("projectblock").innerHTML = out;
 		}
