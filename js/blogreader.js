994196a84bf0b816fe3d739c2f1fb5d8e866f435
Anindya Biswas <myself@anindyabiswas.io>
2018-06-14 13:20:00 +0530

    Resume Section Update
    Trying to get resume to display

--- js/blogreader.js
+++ js/blogreader.js
@@ -14,7 +14,6 @@
 			var i;
 			var out = "";
 			for (i = 0; i < arr.length; i++) {
-
 				out += "<div class=\"card horizontal\"><div class=\"card-stacked\"><div class=\"card-content\">"
 				    +"<div><h5 class=\"header\">"+ arr[i].Title+ "</h5><h6>"+ arr[i].Date+ "</h6><br>"
 						+ decodeURI(arr[i].BlogEncoded)
